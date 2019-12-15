271954e58cd44fa4823b33eeb89b55c64f3b3e2b
Brad Morgan <bkmorgan3@gmail.com>
2019-12-15 14:56:35 -0800

    renders an img

--- client/components/Cafe.jsx
+++ client/components/Cafe.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+// declare proptypes use isRequired
+
 export default function Cafe(props) {
   console.log("props", props.cafes);
   let cafes = props.cafes;
+  const names = [];
 
   for (let i = 0; i < cafes.length; i++) {
     console.log(cafes[i]);
+    let img = <img src={cafes[i].shopImg} alt="" />;
+    names.push(img);
   }
 
   return (
     <div>
       <p>im a cafe</p>
+      {names}
     </div>
   );
 }
