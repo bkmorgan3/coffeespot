21ed5e3b444edc9132d85963f30921ec1c8fb9c8
Brad Morgan <bkmorgan3@gmail.com>
2019-12-22 02:21:23 -0800

    changed this component to render

--- client/components/Cafe.jsx
+++ client/components/Cafe.jsx
@@ -5,18 +5,21 @@ import React from "react";
 export default function Cafe(props) {
   console.log("props", props.cafes);
   let cafes = props.cafes;
-  const names = [];
+  const spots = [];
 
   for (let i = 0; i < cafes.length; i++) {
-    console.log(cafes[i]);
+    console.log("c", cafes[i]);
     let img = <img src={cafes[i].shopImg} alt="" />;
-    names.push(img);
+    spots.push(cafes[i]);
   }
-
+  console.log("spots", spots);
   return (
     <div>
       <p>im a cafe</p>
-      {names}
+      {spots.map(i => {
+        <h4>{i.shopName}</h4>;
+        <img src={i.shopImg} alt="" />;
+      })}
     </div>
   );
 }
